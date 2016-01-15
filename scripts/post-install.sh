f88e54e86d6929aa68583fcfc1fb6a66edffc9d8
Mike Barkas <mike@barkas.com>
2016-01-15 16:22:04 -0500

    Setup install Drupal script.
      Script to use Drush commands to invoke site-install and to enable
      a few modules.

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# --------------------------------------
+# Site configuration.
+# --------------------------------------
+# Site.
+SITE_NAME='Composed Drupal'
+SITE_MAIL='site@email.no'
+
+# User.
+ACCOUNT_MAIL='name@email.no'
+ACCOUNT_PASS='password'
+
+# Database.
+DB_USER='username'
+DB_PASS='password'
+DB_NAME='composed_drupal'
+DB_URL='127.0.0.1'
+# --------------------------------------
+
 TEXT_FILES=(
   "CHANGELOG.txt"
   "COPYRIGHT.txt"
@@ -12,8 +30,26 @@ TEXT_FILES=(
   "UPGRADE.txt"
 )
 
+#
 # Remove text files.
+#
 for FILE in "${TEXT_FILES[@]}"
 do
   rm www/"${FILE}"
 done
+
+#
+# Move into website directory to use Drush.
+#
+cd www
+
+#
+# Drush site install.
+#
+drush si --account-mail=${ACCOUNT_MAIL} --site-mail=${SITE_MAIL} --account-pass=${ACCOUNT_PASS} --site-name=${SITE_NAME} --db-url=mysql://${DB_USER}:${DB_PASS}@${DB_URL}/${DB_NAME} -y
+
+#
+# Enable/disable modules.
+drush dis overlay -y
+drush en views views_ui -y
+
