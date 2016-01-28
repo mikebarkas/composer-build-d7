103cd1d4c24c277be89e948c8714209ab0e4c60e
Mike Barkas <mike@barkas.com>
2016-01-28 13:45:47 -0500

    Adding timestamp to database update filename.

--- scripts/backup-database.sh
+++ scripts/backup-database.sh
@@ -5,9 +5,18 @@
 # --------------------------------------
 
 #
-# Make a directory for backup file.
+# Define database directory and file names.
 #
-mkdir database-backup
+DB_DIRECTORY='database-backup'
+TIMESTAMP=$(date "+%Y-%m-%d-%H:%M:%S")
+DB_FILENAME="dbup-${TIMESTAMP}.sql"
+
+#
+# Make a directory for backup files.
+#
+if [[ ! -d "${DB_DIRECTORY}" ]]; then
+  mkdir "${DB_DIRECTORY}"
+fi
 
 #
 # Move to site directory for Drush.
@@ -17,4 +26,4 @@ cd www
 #
 # Backup the database with Drush.
 #
-drush sql-dump --result-file=../database-backup/dbup.sql
+drush sql-dump --result-file=../${DB_DIRECTORY}/${DB_FILENAME}
